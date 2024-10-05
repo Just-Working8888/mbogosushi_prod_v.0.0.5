9ebd727653b0bf24322dd2db72ed9a06528b4432
Bekbolot <justworking8888@gmail.com>
2024-10-05 19:10:08 +0600

    вам

--- admin/src/components/dashboard/index.ts
+++ admin/src/components/dashboard/index.ts
@@ -12,7 +12,7 @@ export {
 } from './default';
 export { RevenueCard, PostsCard, ProjectsCard } from './shared';
 export { CustomerReviewsCard } from './ecommerce';
-export { ProjectsCountCard, ProjectsTable, ClientsTable } from './projects';
+export { ProjectsCountCard, ClientsTable } from './projects';
 export {
   StatsCard as MarketingStatsCard,
   VisitorsChartCard,
--- admin/src/components/dashboard/projects/index.ts
+++ admin/src/components/dashboard/projects/index.ts
@@ -1,3 +1,2 @@
 export { ClientsTable } from './ClientsTable/ClientsTable.tsx';
 export { ProjectsCountCard } from './ProjectsCountCard/ProjectsCountCard.tsx';
-export { ProjectsTable } from './ProjectsTables/ProjectsTable.tsx';
